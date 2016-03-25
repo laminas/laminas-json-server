bad4127c67d93d729e790904fce3cf8f65a01564
Ali Bahman <abn@webit4.me>
2016-03-25 08:45:39 +0000

    Splitting zend-json-server from the zend-json Dump zend-json in
    Updating book
    
    Strip down zend-son (including exceptions for now) and moved the server content up to the src root
    
    Adjusting exceptions
    
    Removing json and json-xml tests
    
    Amending composer's configuration file to represent zend-json-server instead of the zend-json and be able to fetch the forked zend-json from webit4me which has its zend-json-server subcomponent removed
    
    Adjusting failing test by targeting zend-json new namespace, imported as a dependency.
    
    Updating repository's readme file by the existing introduction

---
+++ src/Exception/ErrorException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Zend Framework (http://framework.zend.com/).
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ *
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+namespace Zend\Json\Server\Exception;
+
+/**
+ * Thrown by Zend\Json\Server\Client when a JSON-RPC fault response is returned.
+ */
+class ErrorException extends BadMethodCallException implements ExceptionInterface
+{
+}
