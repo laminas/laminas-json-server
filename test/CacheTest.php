252a26f7e82a5927dcaf21611e2a45420fe3773e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:07:33 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/CacheTest.php
+++ test/CacheTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-json-server for the canonical source repository
- * @copyright https://github.com/laminas/laminas-json-server/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-json-server/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\Json\Server;
 
 use Laminas\Json\Server;
